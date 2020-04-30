0629838a8bd70952f6f578f58ea6eb70f95cdfee
Tom Bell <tjbell@gmail.com>
2020-04-30 12:03:48 -0600

    module4-solution

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,12 @@
 import shutil
+import sys
 
 from typing import List
 from pathlib import Path
+
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 
 class Parser:
@@ -31,3 +36,27 @@ class ResourceParser(Parser):
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
+
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
+
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,5 @@
+import sys
+
 from pathlib import Path
 
 
@@ -21,7 +23,9 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not Implemented")
+            self.error(
+                "No parser for the {} extension, file skipped!".format(path.suffix)
+            )
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
@@ -30,3 +34,7 @@ class Site:
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
