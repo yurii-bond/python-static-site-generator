34219fb50cf9f5c03f8619198cd9374d98622d77
Yurii Bondarenko <BnrY@viessmann.com>
2022-02-06 00:34:03 +0100

    Completed module

---
+++ ssg.py
@@ -0,0 +1,10 @@
+import typer
+from ssg.site import Site
+
+
+def main(source="content", dest="dist"):
+    config = {"source": source, "dest": dest}
+    Site(**config).build()
+
+
+typer.run(main)
