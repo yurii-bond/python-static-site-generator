6b4361623de0768f097d159734856f1da8843c8c
Yurii Bondarenko <BnrY@viessmann.com>
2022-02-07 14:08:09 +0100

    Completed module

--- ssg.py
+++ ssg.py
@@ -1,9 +1,10 @@
 import typer
+import ssg.parsers
 from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
+    config = {"source": source, "dest": dest, "parsers":[ssg.parsers.ResourceParser()]}
     Site(**config).build()
 
 
